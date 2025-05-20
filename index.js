814e331f63785f29cc826fbbb3c07f4238497ab6
LJ-Fryer <ljf_aus@hotmail.com>
2025-05-20 14:38:09 +1000

    Added key sequence  and secret code functionalities

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Testing");
+const pressed = [];
+const secretCode = "wtfiuwy";
+
+window.addEventListener("keyup", (e) => {
+  pressed.push(e.key); // Builds an array of the keys struck
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); //Trim to the maximum length of the secret code
+  console.log(pressed);
+
+  // Check for secret code
+  if (pressed.join("").includes(secretCode)) {
+    console.log("Got it");
+    cornify_add();
+  }
+});
